b5e7f7daa3c3c28f3266feb34fbeaa3f98f9b204
JoeDictionary <filipfit6@gmail.com>
2020-10-20 16:51:50 +0200

    sync

--- src/app/stay-card/stay-card.component.ts
+++ src/app/stay-card/stay-card.component.ts
@@ -1,4 +1,6 @@
+import { Stay } from './stay.interface';
 import { Component, Input, OnInit } from '@angular/core';
+import stayData from '../../example_data/stays.json';
 
 @Component({
   selector: 'stay-card',
@@ -6,10 +8,13 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./stay-card.component.scss'],
 })
 export class StayCardComponent implements OnInit {
+  @Input('superhost') superhost: boolean;
+  @Input('data') data: Stay = stayData[2];
+
   constructor() {}
-  @Input('superhost') superhost = false;
 
   // TODO Create component using content projection or @Input-properties????????
-
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log();
+  }
 }
